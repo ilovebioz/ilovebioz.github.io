9ae9f9a4910533f1713d143b201a288657b027d1
sectic <ilovebioz@gmail.com>
2019-08-30 08:52:54 +0700

    Updates

---
+++ precache-manifest.c9d340908abbd306383528f3239dbc94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53c3026479f564162b8449428618afd",
+    "url": "/sectic/index.html"
+  },
+  {
+    "revision": "1c176e93ffc458bf642f",
+    "url": "/sectic/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "e919af2991c2e4e93618",
+    "url": "/sectic/static/js/2.eed3406c.chunk.js"
+  },
+  {
+    "revision": "1c176e93ffc458bf642f",
+    "url": "/sectic/static/js/main.3d118511.chunk.js"
+  },
+  {
+    "revision": "e58734f0b037f25ad333",
+    "url": "/sectic/static/js/runtime~main.27df9a0b.js"
+  }
+]);
