df2580736f15d756c82739f1d0fbf6f9d851bf61
sectic <ilovebioz@gmail.com>
2020-04-07 16:49:31 +0700

    Updates

---
+++ precache-manifest.3d3e690ca225a618dedbc6ff659505a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd9cd1b1577a6bfd120a5f385e19e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1dbea06145d71821d5c",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "e919af2991c2e4e93618",
+    "url": "/static/js/2.eed3406c.chunk.js"
+  },
+  {
+    "revision": "b1dbea06145d71821d5c",
+    "url": "/static/js/main.ec8461d3.chunk.js"
+  },
+  {
+    "revision": "c2f24a53908eae11e460",
+    "url": "/static/js/runtime~main.38bac7db.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sectic/precache-manifest.cac48b135072efffaaa854363178995c.js"
+  "/precache-manifest.3d3e690ca225a618dedbc6ff659505a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sectic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
